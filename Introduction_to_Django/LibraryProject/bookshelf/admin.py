0f2863e5fc6a5b732a4bdd071da9548710bc51b1
AureliaBett <cbettaurelia@gmail.com>
2025-11-02 23:02:53 +0300

    Admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # columns to show in the changelist
+    list_display = ("id", "title", "author", "publication_year")
+    # fields that can be edited directly in the changelist (optional)
+    list_editable = ("publication_year",)
+    # quick filters in the right sidebar
+    list_filter = ("publication_year", "author")
+    # fields to search by using the search box
+    search_fields = ("title", "author")
+    # default ordering
+    ordering = ("-publication_year", "title")
+    # number of items per page in changelist
+    list_per_page = 25
+
