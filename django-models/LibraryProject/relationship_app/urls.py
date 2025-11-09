e148a0b8a902e156ada0c1f1cdf13c4516ebca36
AureliaBett <cbettaurelia@gmail.com>
2025-11-09 09:07:59 +0300

    User Authentification

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .import views
-from .views import list_books
+from .views import RegistrationView, LoginView, LogoutView, list_books, LibraryDetailView
 
 urlpatterns=[
     path('', views.list_books, name='home'),
-    path('list_books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),]
+    path('list_books/', list_books, name='list_books'),
+    path('LibraryDetailView/', LibraryDetailView.as_view(), name='LibraryDetailView'),
+    
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', RegistrationView.as_view(), name='register'),
+    
+    ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+from django.urls import path
 
 # Create your views here.
 from django.views.generic.detail import DetailView
@@ -20,3 +26,15 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context["books"] = self.object.books.all()
         return context
+
+#User Registration
+class RegistrationView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
+
+#User Login
+urlpatterns = [
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name = 'relationship_app/login.html'), name='logout'),
+]
