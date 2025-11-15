e222e3e207d524a2e38c9a0dadf805931aa5e589
AureliaBett <cbettaurelia@gmail.com>
2025-11-15 07:46:52 +0300

    Security Best Practices In Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,42 +1,57 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import BookForm
 
-# View a list of books
+
+# List Books
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()
+    books = Book.objects.all()  # ORM protects from SQL injection
     return render(request, 'bookshelf/book_list.html', {'books': books})
 
-# Add a new book
+
+# Create Book (Secure)
 @permission_required('bookshelf.can_create', raise_exception=True)
 def book_create(request):
-    if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        year = request.POST.get("publication_year")
-        Book.objects.create(title=title, author=author, publication_year=year)
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html')
+    if request.method == 'POST':
+        form = BookForm(request.POST)
 
-# Edit an existing book
+        if form.is_valid():         # Validates + sanitizes input
+            form.save()
+            return redirect('book_list')
+
+    else:
+        form = BookForm()
+
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+
+# Edit Book (Secure)
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def book_edit(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    if request.method == "POST":
-        book.title = request.POST.get("title")
-        book.author = request.POST.get("author")
-        book.publication_year = request.POST.get("publication_year")
-        book.save()
-        return redirect('book_list')
-    return render(request, 'bookshelf/book_form.html', {'book': book})
 
-# Delete a book
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+
+    else:
+        form = BookForm(instance=book)
+
+    return render(request, 'bookshelf/book_form.html', {'form': form})
+
+
+# Delete Book (Secure)
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def book_delete(request, book_id):
     book = get_object_or_404(Book, id=book_id)
-    book.delete()
-    return redirect('book_list')
+
+    if request.method == "POST":  # Prevent accidental deletes via GET
+        book.delete()
+        return redirect('book_list')
+
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
