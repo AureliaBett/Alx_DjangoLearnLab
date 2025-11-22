83a119fba0c0b8131255e1dd40a503de248c7b7b
AureliaBett <cbettaurelia@gmail.com>
2025-11-22 06:53:03 +0300

    Authenticatqons and Permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from .models import Book
 from .serializers import BookSerializer
 
+
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
-    serializer_class =BookSerializer
+    serializer_class = BookSerializer
+
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer_class =BookSerializer
+    serializer_class = BookSerializer
+
+   
+    permission_classes = [IsAuthenticated]
+
+   
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
