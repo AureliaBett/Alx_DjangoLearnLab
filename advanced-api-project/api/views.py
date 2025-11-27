e4773180bb3da1514f19302a6689a9486b78adcd
AureliaBett <cbettaurelia@gmail.com>
2025-11-27 20:37:43 +0300

    Add Permissions and validations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,26 +2,59 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import generics
+from rest_framework import generics, permissions, status, filters
+from rest_framework.response import Response
 from .models import Book, Author
 from .serializers import BookSerializer
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
+    permission_classes = [permissions.AllowAny]  # Anyone can view
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'author__name']
 
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
+    permission_classes = [permissions.AllowAny]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer 
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({
+            "message": "Book successfully created!",
+            "book": serializer.data
+        }, status=status.HTTP_201_CREATED, headers=headers)
+
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)  
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            "message": "Book successfully updated!",
+            "book": serializer.data
+        }, status=status.HTTP_200_OK)
+
 class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer  
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({
+            "message": "Book successfully deleted!"
+        }, status=status.HTTP_204_NO_CONTENT)
