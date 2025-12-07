858998b4ad57b29fc08710d1e884c5b3ce64b759
AureliaBett <cbettaurelia@gmail.com>
2025-12-07 12:03:55 +0300

    Chane posts tp post in urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(template_name="registration/logout_out.html"), name="logout"),
     path("register/", register_view, name="register"),
     path("profile/", profile_view, name="profile"),
-    path('posts/', PostListView.as_view(), name='post-list'),                   # List all posts
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),           # Create a new post
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),      # View post details
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'), # Update a post
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete a post
+    path('post/', PostListView.as_view(), name='post-list'),                   # List all posts
+    path('post/new/', PostCreateView.as_view(), name='post-create'),           # Create a new post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),      # View post details
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'), # Update a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete a post
 ]
