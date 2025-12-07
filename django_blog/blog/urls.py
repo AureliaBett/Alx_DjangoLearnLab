d025cceed37079436411546f23f2c631142c4b2e
AureliaBett <cbettaurelia@gmail.com>
2025-12-07 12:27:53 +0300

    added comments/new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'), # Update a post
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'), # Delete a post
     path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', PostDetailView.as_view(), name='post-detail'),
     path('comment/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
