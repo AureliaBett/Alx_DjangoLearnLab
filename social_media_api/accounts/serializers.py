d677cfb1417e02b6416c2da697b61538f6ce1941
AureliaBett <cbettaurelia@gmail.com>
2025-12-11 23:16:43 +0300

    Create a user using get_user_model()

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     
     
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
             password=validated_data['password'],
