0e9c6bdfff09d2cce9dfb0f36fca96414a4a9443
AureliaBett <cbettaurelia@gmail.com>
2025-12-12 00:55:55 +0300

    Added views and serializers for post and comments

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -4,16 +4,37 @@ from .models import Post, Comment
 
 User = settings.AUTH_USER_MODEL
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'author']
-        
 
 
-class CommentSerializer(serializers.Serializer):
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')  # display author's username
+
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'created_at', 'updated_at', 'author', 'post']
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        # Set author from request user
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            validated_data['author'] = request.user
+        return super().create(validated_data)
 
 
+# Serializer for Posts
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    comments = CommentSerializer(many=True, read_only=True)  # include related comments
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'author_username', 'created_at', 'updated_at', 'comments']
+        read_only_fields = ['author', 'created_at', 'updated_at']
+
+    def create(self, validated_data):
+        # Set author from request user
+        request = self.context.get('request')
+        if request and hasattr(request, 'user'):
+            validated_data['author'] = request.user
+        return super().create(validated_data)
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+
+# Custom permission: only author can edit/delete
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow authors of an object to edit or delete it.
+    Read permissions are allowed to any request.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only for the author
+        return obj.author == request.user
+
+
+# Post ViewSet
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Set the author to the logged-in user
+        serializer.save(author=self.request.user)
+
+
+# Comment ViewSet
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
